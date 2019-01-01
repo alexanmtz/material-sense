25a86cef88b1443964c0bd40df099821cf00ee9a
Alexandre Magno Teles Zimerer <alz@danskebank.dk>
2019-01-01 21:58:36 +0100

    Updates

---
+++ precache-manifest.3423bd73f16d288aff6e1fdb8bd5ff69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e6f5b7c8c551aa716df",
+    "url": "/material-sense/static/css/main.25621c15.chunk.css"
+  },
+  {
+    "revision": "4e6f5b7c8c551aa716df",
+    "url": "/material-sense/static/js/main.4e6f5b7c.chunk.js"
+  },
+  {
+    "revision": "f51cfafb1fa7c033293d",
+    "url": "/material-sense/static/js/1.f51cfafb.chunk.js"
+  },
+  {
+    "revision": "09f4e86c218a577ad5da",
+    "url": "/material-sense/static/js/runtime~main.09f4e86c.js"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/material-sense/static/media/shape.208f3d38.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-sense/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df4501001ded47e74f7f2d7b97c768fd",
+    "url": "/material-sense/index.html"
+  }
+];

fba2358c42ad3a5c19cc0cb5c047b54d57a7f2be
Alexandre Magno <alz@danskebank.dk>
2019-07-01 13:44:20 +0200

    Updates

---
+++ precache-manifest.22d0aa4c15d043612ee83d8c11ddb6e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8957bffe3f237da9e5ec253e00b2480a",
+    "url": "/material-sense/index.html"
+  },
+  {
+    "revision": "8925ab1fb1cad4e18a5b",
+    "url": "/material-sense/static/css/main.68a98085.chunk.css"
+  },
+  {
+    "revision": "906d98efcd5aec7b0567",
+    "url": "/material-sense/static/js/2.031cb5a5.chunk.js"
+  },
+  {
+    "revision": "8925ab1fb1cad4e18a5b",
+    "url": "/material-sense/static/js/main.7bf1a11d.chunk.js"
+  },
+  {
+    "revision": "934102074e734d235b58",
+    "url": "/material-sense/static/js/runtime~main.55b3dbbc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-sense/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/material-sense/static/media/shape.208f3d38.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-sense/precache-manifest.9f51f3f2399701e20fc31f15bd2c5dde.js"
+  "/material-sense/precache-manifest.22d0aa4c15d043612ee83d8c11ddb6e9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/material-sense/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/material-sense/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

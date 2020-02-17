5714e530d795db433fe329fe25cf267130f69545
angelorubin <angelorubin@gmail.com>
2020-02-16 23:09:10 -0300

    fixing warnings after updating to version 4 of the ui-material, more information about the error can be found here - https://bit.ly/2SxrXSb

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,20 @@
-import React,  { Component } from 'react';
-import withStyles from '@material-ui/styles/withStyles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import CardItem from './cards/CardItem';
-import Topbar from './Topbar';
-import SectionHeader from './typo/SectionHeader';
-const backgroundShape = require('../images/shape.svg');
+import React, { Component } from "react";
+import withStyles from "@material-ui/styles/withStyles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import CardItem from "./cards/CardItem";
+import Topbar from "./Topbar";
+import SectionHeader from "./typo/SectionHeader";
+const backgroundShape = require("../images/shape.svg");
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.grey['A500'],
-    overflow: 'hidden',
+    backgroundColor: theme.palette.grey["A500"],
+    overflow: "hidden",
     background: `url(${backgroundShape}) no-repeat`,
-    backgroundSize: 'cover',
-    backgroundPosition: '0 400px',
+    backgroundSize: "cover",
+    backgroundPosition: "0 400px",
     marginTop: 20,
     padding: 20,
     paddingBottom: 200
@@ -22,30 +22,38 @@ const styles = theme => ({
   grid: {
     width: 1000
   }
-})
+});
 
 class Cards extends Component {
-
   render() {
     const { classes } = this.props;
-    const currentPath = this.props.location.pathname
+    const currentPath = this.props.location.pathname;
 
     return (
       <React.Fragment>
         <CssBaseline />
         <Topbar currentPath={currentPath} />
         <div className={classes.root}>
-          <Grid container justify="center"> 
-            <Grid spacing={24} alignItems="center" justify="center" container className={classes.grid}>
+          <Grid container justify="center">
+            <Grid
+              spacing={10}
+              alignItems="center"
+              justify="center"
+              container
+              className={classes.grid}
+            >
               <Grid item xs={12}>
-                <SectionHeader title="Cards" subtitle="One page with a list of a collection" />
+                <SectionHeader
+                  title="Cards"
+                  subtitle="One page with a list of a collection"
+                />
                 <CardItem />
               </Grid>
             </Grid>
           </Grid>
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,97 +1,96 @@
-import React,  { Component } from 'react';
-import withStyles from '@material-ui/styles/withStyles';
-import { withRouter } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import InstructionDialog from './dialogs/InstructionDialog';
-import SwipeDialog from './dialogs/SwipeDialog';
+import React, { Component } from "react";
+import withStyles from "@material-ui/styles/withStyles";
+import { withRouter } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import InstructionDialog from "./dialogs/InstructionDialog";
+import SwipeDialog from "./dialogs/SwipeDialog";
 
-import Topbar from './Topbar';
+import Topbar from "./Topbar";
 
-const backgroundShape = require('../images/shape.svg');
+const backgroundShape = require("../images/shape.svg");
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.grey['100'],
-    overflow: 'hidden',
+    backgroundColor: theme.palette.grey["100"],
+    overflow: "hidden",
     background: `url(${backgroundShape}) no-repeat`,
-    backgroundSize: 'cover',
-    backgroundPosition: '0 400px',
+    backgroundSize: "cover",
+    backgroundPosition: "0 400px",
     paddingBottom: 200
   },
   grid: {
     width: 1200,
     marginTop: 40,
-    [theme.breakpoints.down('sm')]: {
-      width: 'calc(100% - 20px)'
+    [theme.breakpoints.down("sm")]: {
+      width: "calc(100% - 20px)"
     }
   },
   paper: {
     padding: theme.spacing(3),
-    textAlign: 'left',
-    color: theme.palette.text.secondary,
+    textAlign: "left",
+    color: theme.palette.text.secondary
   },
   rangeLabel: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    display: "flex",
+    justifyContent: "space-between",
     paddingTop: theme.spacing(2)
   },
   topBar: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginTop: 32
   },
   outlinedButtom: {
-    textTransform: 'uppercase',
+    textTransform: "uppercase",
     margin: theme.spacing(1)
   },
   actionButtom: {
-    textTransform: 'uppercase',
+    textTransform: "uppercase",
     margin: theme.spacing(1),
     width: 152
   },
   blockCenter: {
     padding: theme.spacing(2),
-    textAlign: 'center'
+    textAlign: "center"
   },
   block: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(2)
   },
   box: {
     marginBottom: 40,
     height: 65
   },
   inlining: {
-    display: 'inline-block',
+    display: "inline-block",
     marginRight: 10
   },
   buttonBar: {
-    display: 'flex'
+    display: "flex"
   },
   alignRight: {
-    display: 'flex',
-    justifyContent: 'flex-end'
+    display: "flex",
+    justifyContent: "flex-end"
   },
   noBorder: {
-    borderBottomStyle: 'hidden'
+    borderBottomStyle: "hidden"
   },
   loadingState: {
     opacity: 0.05
   },
   loadingMessage: {
-    position: 'absolute',
-    top: '40%',
-    left: '40%'
+    position: "absolute",
+    top: "40%",
+    left: "40%"
   }
 });
 
 class Main extends Component {
-
   state = {
     learnMoredialog: false,
     getStartedDialog: false
@@ -99,21 +98,21 @@ class Main extends Component {
 
   componentDidMount() {}
 
-  openDialog = (event) => {
-    this.setState({learnMoredialog: true});
-  }
+  openDialog = event => {
+    this.setState({ learnMoredialog: true });
+  };
 
-  dialogClose = (event) => {
-    this.setState({learnMoredialog: false});
-  }
+  dialogClose = event => {
+    this.setState({ learnMoredialog: false });
+  };
 
-  openGetStartedDialog = (event) => {
-    this.setState({getStartedDialog: true});
-  }
+  openGetStartedDialog = event => {
+    this.setState({ getStartedDialog: true });
+  };
 
-  closeGetStartedDialog = (event) => {
-    this.setState({getStartedDialog: false});
-  }
+  closeGetStartedDialog = event => {
+    this.setState({ getStartedDialog: false });
+  };
 
   render() {
     const { classes } = this.props;
@@ -123,19 +122,33 @@ class Main extends Component {
         <Topbar />
         <div className={classes.root}>
           <Grid container justify="center">
-            <Grid spacing={4} alignItems="center" justify="center" container className={classes.grid}>
+            <Grid
+              spacing={4}
+              alignItems="center"
+              justify="center"
+              container
+              className={classes.grid}
+            >
               <Grid item xs={12} md={4}>
                 <Paper className={classes.paper}>
                   <div className={classes.box}>
-                    <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
+                    <Typography
+                      style={{ textTransform: "uppercase" }}
+                      color="secondary"
+                      gutterBottom
+                    >
                       First title
                     </Typography>
                     <Typography variant="body2" gutterBottom>
-                      A first title style <br/> with two lines
+                      A first title style <br /> with two lines
                     </Typography>
                   </div>
-                  <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <Button color='primary' variant="contained" className={classes.actionButtom}>
+                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      className={classes.actionButtom}
+                    >
                       Learn more
                     </Button>
                   </div>
@@ -144,15 +157,23 @@ class Main extends Component {
               <Grid item xs={12} md={4}>
                 <Paper className={classes.paper}>
                   <div className={classes.box}>
-                    <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
+                    <Typography
+                      style={{ textTransform: "uppercase" }}
+                      color="secondary"
+                      gutterBottom
+                    >
                       Another box
                     </Typography>
                     <Typography variant="body1" gutterBottom>
                       A default box
                     </Typography>
                   </div>
-                  <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <Button color='primary' variant="contained" className={classes.actionButtom}>
+                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      className={classes.actionButtom}
+                    >
                       Learn more
                     </Button>
                   </div>
@@ -161,56 +182,75 @@ class Main extends Component {
               <Grid item xs={12} md={4}>
                 <Paper className={classes.paper}>
                   <div className={classes.box}>
-                    <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
+                    <Typography
+                      style={{ textTransform: "uppercase" }}
+                      color="secondary"
+                      gutterBottom
+                    >
                       A box with a carousel
                     </Typography>
                     <Typography variant="body1" gutterBottom>
-                      If you click in Getting Started, you will see a nice carousel
+                      If you click in Getting Started, you will see a nice
+                      carousel
                     </Typography>
                   </div>
                   <div className={classes.alignRight}>
-                    <Button onClick={this.openDialog}  variant="outlined" className={classes.actionButtom}>
+                    <Button
+                      onClick={this.openDialog}
+                      variant="outlined"
+                      className={classes.actionButtom}
+                    >
                       Learn more
                     </Button>
-                    <Button onClick={this.openGetStartedDialog} color='primary' variant="contained" className={classes.actionButtom}>
+                    <Button
+                      onClick={this.openGetStartedDialog}
+                      color="primary"
+                      variant="contained"
+                      className={classes.actionButtom}
+                    >
                       Dashboard
                     </Button>
                   </div>
                 </Paper>
               </Grid>
               <Grid container item xs={12}>
-                  <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                      <div>
-                        <div className={classes.box}>
-                          <Typography color='secondary' gutterBottom>
-                            Full box
-                          </Typography>
-                          <Typography variant="body1" gutterBottom>
-                            This is an example of a full-width box
-                          </Typography>
-                        </div>
-                        <div className={classes.alignRight}>
-                          <Button color='primary' variant="contained" className={classes.actionButtom}>
-                            Learn more
-                          </Button>
-                        </div>
+                <Grid item xs={12}>
+                  <Paper className={classes.paper}>
+                    <div>
+                      <div className={classes.box}>
+                        <Typography color="secondary" gutterBottom>
+                          Full box
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                          This is an example of a full-width box
+                        </Typography>
                       </div>
-                    </Paper>
+                      <div className={classes.alignRight}>
+                        <Button
+                          color="primary"
+                          variant="contained"
+                          className={classes.actionButtom}
+                        >
+                          Learn more
+                        </Button>
+                      </div>
+                    </div>
+                  </Paper>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
           <SwipeDialog
             open={this.state.learnMoredialog}
-            onClose={this.dialogClose} />
+            onClose={this.dialogClose}
+          />
           <InstructionDialog
             open={this.state.getStartedDialog}
             onClose={this.closeGetStartedDialog}
           />
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
